a6f857497e847aa6ba071df849d279f12c863937
HarasawaNaoya <rururulily@gmail.com>
2020-08-25 19:20:09 +0900

    ビルドページ

---
+++ docs/web-mobile/src/settings.2c98f.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/GameMain.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",main:"7d50b"}};
